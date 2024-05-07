b4cfdff1bbea8b4032f5f5ad23da1ce91e26f43b
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2024-05-07 08:08:34 +0000

    Add login, signup, and forgot password modals.

--- webapp/src/components/Navbar.svelte
+++ webapp/src/components/Navbar.svelte
@@ -15,6 +15,18 @@
     width: 100%;
     padding: 0;
   }
+
+  ul {
+    align-items: center;
+  }
+
+  .btn-link {
+    color: var(--qudcolor-y);
+  }
+
+  .modal-input {
+    color: black;
+  }
 </style>
 
 <div class="nav-background">
@@ -42,13 +54,9 @@
       <div class="collapse navbar-collapse" id="navbarToggler">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="#">Stats</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Search</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Login</a>
+            <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#loginModalToggle">
+              Login
+            </button>
           </li>
         </ul>
       </div>
@@ -56,3 +64,93 @@
   </div>
 </div>
 
+<!-- Login modal -->
+<div class="modal fade" id="loginModalToggle" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <span class="modal-title fs-5 fw-bold" id="exampleModalLabel">Login to Hagiography</span>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="email" class="col-form-label">Email:</label>
+            <input type="text" class="form-control modal-input" id="email">
+          </div>
+          <div class="mb-3">
+            <label for="message-text" class="col-form-label">Password:</label>
+            <input type="password" class="form-control modal-input" id="password">
+          </div>
+        </form>
+
+        <button class="btn btn-link" data-bs-target="#forgotPasswordModalToggle" data-bs-toggle="modal">
+          I forgot my password
+        </button>
+        <button class="btn btn-link" data-bs-target="#signupModalToggle" data-bs-toggle="modal">
+          Create a new account
+        </button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Login</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Signup modal -->
+<div class="modal fade" id="signupModalToggle" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <span class="modal-title fs-5 fw-bold" id="exampleModalLabel">Sign up for Hagiography</span>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="email" class="col-form-label">Email:</label>
+            <input type="text" class="form-control modal-input" id="email">
+          </div>
+        </form>
+
+        <button class="btn btn-link" data-bs-target="#loginModalToggle" data-bs-toggle="modal">
+          Return to login
+        </button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send signup link</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Forgot password modal -->
+<div class="modal fade" id="forgotPasswordModalToggle" tabindex="-1" aria-labelledby="forgotPasswordModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <span class="modal-title fs-5 fw-bold" id="exampleModalLabel">Login to Hagiography</span>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form>
+          <div class="mb-3">
+            <label for="email" class="col-form-label">Email:</label>
+            <input type="text" class="form-control modal-input" id="email">
+          </div>
+        </form>
+
+        <button class="btn btn-link" data-bs-target="#loginModalToggle" data-bs-toggle="modal">
+          Return to login
+        </button>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Send password reset link</button>
+      </div>
+    </div>
+  </div>
+</div>
