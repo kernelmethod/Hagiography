06af43e72bc7e43374b36712a384de7edd18f423
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2024-05-07 00:32:01 +0000

    Updates to the User model
    - Make integer ID the primary key field, rather than email
    - Add a "created" timestamp to users, to see when they were added
      to the database.

--- server/api/models/user.py
+++ server/api/models/user.py
@@ -1,12 +1,20 @@
+from datetime import datetime, timezone
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+def current_time():
+    return datetime.now(timezone.utc)
+
+
 class User(AbstractUser):
 
-    email = models.EmailField(primary_key=True, unique=True, null=False)
+    id = models.AutoField(primary_key=True)
+
+    email = models.EmailField(unique=True, null=False)
     username = models.CharField(max_length=64, unique=True, null=False)
     password = models.CharField(max_length=256, null=False)
+    created = models.DateTimeField(default=current_time, null=False)
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
