13578ba1ea93ea724d9552efe6b7e60b97363cf1
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2024-05-08 01:55:10 +0000

    Use MessagePack rather than json for tokens
    Use MessagePack to serialize token information, instead of using JSON.
    The new tokens are significantly smaller (and hopefully easier to review
    / type in manually, if absolutely need be).

--- server/api/auth/tokens.py
+++ server/api/auth/tokens.py
@@ -1,5 +1,6 @@
-import json
+from __future__ import annotations
 import pyseto
+import msgpack
 from api.models import User
 from datetime import datetime, timedelta, timezone
 from django.conf import settings
@@ -15,18 +16,31 @@ class TokenPayload(BaseModel):
     epoch: int
     scopes: list[str] = Field(default_factory=lambda: list())
 
+    def serialize(self) -> bytes:
+        return msgpack.packb([self.user_id, self.epoch, self.scopes])
+
+    @classmethod
+    def deserialize(cls, input: bytes) -> TokenPayload:
+        user_id, epoch, scopes = msgpack.unpackb(input, use_list=False, raw=False)
+        return cls(user_id=user_id, epoch=epoch, scopes=scopes)
+
 
 class TokenFooter(BaseModel):
     expires: datetime
 
+    def serialize(self) -> bytes:
+        return msgpack.packb([self.expires.timestamp()])
+
+    @classmethod
+    def deserialize(cls, input: bytes) -> TokenFooter:
+        (ts,) = msgpack.unpackb(input, use_list=False, raw=False)
+        expires = datetime.fromtimestamp(ts, tz=timezone.utc)
+        return cls(expires=expires)
+
 
 class TokenFactory:
 
-    KNOWN_SCOPES: set[str] = {
-        "upload",
-        "test__user",
-        "test__admin"
-    }
+    KNOWN_SCOPES: set[str] = {"upload", "test__user", "test__admin"}
 
     def __init__(self) -> None:
         self.key = pyseto.Key.new(version=4, purpose="local", key=settings.TOKEN_KEY)
@@ -50,8 +64,8 @@ class TokenFactory:
         elif expires_delta is not None:
             footer.expires = datetime.now(timezone.utc) + expires_delta
 
-        payload_json = payload.model_dump_json().encode("utf-8")
-        footer_json = footer.model_dump_json().encode("utf-8")
+        payload_json = payload.serialize()
+        footer_json = footer.serialize()
 
         token = pyseto.encode(self.key, payload=payload_json, footer=footer_json)
 
@@ -63,12 +77,12 @@ class TokenFactory:
         except ValueError:
             raise TokenValidationError("invalid token")
 
-        footer = TokenFooter(**json.loads(decoded.footer.decode()))
+        footer = TokenFooter.deserialize(decoded.footer)
 
         if datetime.now(timezone.utc) > footer.expires:
             raise TokenValidationError("expired token")
 
-        payload = TokenPayload(**json.loads(decoded.payload.decode()))
+        payload = TokenPayload.deserialize(decoded.payload)
         if scopes is not None:
             if any(filter(lambda s: s not in payload.scopes, scopes)):
                 raise TokenValidationError("insufficient permissions")
