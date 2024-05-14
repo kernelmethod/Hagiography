2fca3d5b68a4a5ffd013ec8d701a0360a2f00b00
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2024-05-14 23:16:29 +0000

    Add data: URIs to the allowed img-srcs in the CSP

--- webapp/svelte.config.js
+++ webapp/svelte.config.js
@@ -24,6 +24,7 @@ export default {
             directives: {
                 'default-src': ['self'],
                 'style-src': ['self', 'unsafe-inline'],
+                'img-src': ['self', 'data:'],
             },
         }
     }
