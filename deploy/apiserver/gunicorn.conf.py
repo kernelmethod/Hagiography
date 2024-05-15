32acb47501bece2537b0f4f455644e95f92611b5
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2024-05-15 01:38:25 +0000

    Compress static content served by Caddy.

--- deploy/apiserver/gunicorn.conf.py
+++ deploy/apiserver/gunicorn.conf.py
@@ -7,6 +7,11 @@ bind = "0.0.0.0:8000"
 workers = 2 * cpu_count() + 3
 reload = bool(int(os.getenv("GUNICORN_RELOAD", 0)))
 
+if reload:
+    print("Running server with --reload enabled")
+else:
+    print("Running server witout --reload enabled")
+
 # Logging configuration
 accesslog = "-"
 errorlog = "-"
