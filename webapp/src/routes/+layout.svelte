48196270c8639e727cae288ccf16a23b48268231
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2024-05-15 00:45:08 +0000

    Bring in Bootstrap dynamically via yarn.

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -1,6 +1,14 @@
 <script>
+  import bootstrapCss from 'bootstrap/dist/css/bootstrap.min.css?url';
+  import bootstrapSrc from 'bootstrap/dist/js/bootstrap.min.js?url';
+
   import Navbar from "$components/Navbar.svelte";
 </script>
+
+<svelte:head>
+  <link rel="stylesheet" href={bootstrapCss}>
+  <script src={bootstrapSrc}></script>
+</svelte:head>
 
 <Navbar />
 
