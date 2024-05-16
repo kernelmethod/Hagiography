1712d63906bfbbbfb9030540992ee75d22df56fc
kernelmethod <17100608+kernelmethod@users.noreply.github.com>
2024-05-16 08:25:01 +0000

    Start adding pagination and more mobile-friendly record table

--- webapp/src/components/DateTime.svelte
+++ webapp/src/components/DateTime.svelte
@@ -1,12 +1,47 @@
 <!-- Read a date and time from a string -->
 <script>
   export let timestamp;
+  export let showDelta = false;
 
-  function parseDate(ts) {
-    return (new Date(1000 * ts)).toLocaleString();
+  function getDelta(date) {
+    const currentTime = Date.now();
+    const seconds = (currentTime - date) / 1000;
+
+    if (seconds < 60)
+      return pluralTime(seconds, 'second');
+
+    const minutes = seconds / 60;
+    if (minutes < 60)
+      return pluralTime(minutes, 'minute');
+
+    const hours = minutes / 60;
+    if (hours < 24)
+      return pluralTime(hours, 'hour');
+
+    const days = hours / 24;
+    if (days < 7)
+      return pluralTime(days, 'day');
+
+    return new Date(currentTime).toLocaleDateString();
+  }
+
+  function pluralTime(n, unit) {
+    n = Math.floor(n);
+
+    if (n > 1)
+      unit = unit + 's';
+
+    return `${n} ${unit} ago`;
+  }
+
+  let date = new Date(1000 * timestamp);
+
+  if (showDelta) {
+    date = getDelta(date);
+  }
+  else {
+    date = date.toLocaleString();
   }
 </script>
 
-<span>
-  {parseDate(timestamp)}
-</span>
+<span>{date}</span>
--- webapp/src/components/RecordTable.svelte
+++ webapp/src/components/RecordTable.svelte
@@ -6,23 +6,36 @@
   import ColorizedText from "$components/ColorizedText.svelte";
   import Spinner from "$components/Spinner.svelte";
 
+  let currentIndex = 0;
+  let count = 0;
+  const numRetrieved = 10;
+
   let waitingForRecords = true;
   let records = null;
   let listRecordsPromise = null;
-  const endpoint = "/api/records/list";
 
   function visitRecord(record) {
     window.location = "/records?id=" + record.id;
   }
 
+  function fetchRecords(startIndex) {
+    if (startIndex < 0)
+      return;
+
+    listRecordsPromise = listRecords(startIndex);
+    currentIndex = startIndex;
+  }
+
   async function listRecords() {
+    const endpoint = `/api/records/list?start=${currentIndex}&end=${currentIndex+numRetrieved}`;
     return await fetch(endpoint)
       .then(response => response.json())
       .then(response => {
+        count = response.count || 0;
         return response.records;
       })
       .catch(err => {
-        console.log("Error retrieving records: " + err);
+        console.error("Error retrieving records: " + err);
         return [];
       })
       .finally(() => {
@@ -30,7 +43,7 @@
       });
   }
 
-  onMount(() => { listRecordsPromise = listRecords(); });
+  onMount(() => { fetchRecords(0); });
 </script>
 
 <style lang="postcss">
@@ -62,66 +75,85 @@
     flex-wrap: wrap;
   }
 
-  .character-name > span {
-    max-width: 400px;
-    padding-left: 1rem;
+  button:disabled {
+    opacity: 0.5;
+  }
+
+  button:disabled:hover {
+    background-color: inherit;
   }
 </style>
 
-<div class="flex my-4 justify-center">
-  <table class="w-10/12 border-x border-b">
-    <thead class="text-left">
-      <tr>
-        <th scope="col">Game mode</th>
-        <th scope="col">Character</th>
-        <th class="hidden lg:table-cell" scope="col">Score</th>
-        <th class="hidden lg:table-cell" scope="col">Turns</th>
-        <th scope="col">Uploaded</th>
-      </tr>
-    </thead>
-    <tbody class="text-left">
-      {#if waitingForRecords}
-      <!-- Records haven't been retrieved yet -->
-      <tr>
-        <td class="text-center" colspan="5">
-          <Spinner>
-            Loading game records...
-          </Spinner>
-        </td>
-      </tr>
-      {/if}
-      {#if listRecordsPromise !== null}
-      {#await listRecordsPromise then records}
-      {#if records.length === 0}
-      <tr>
-        <td class="multicol-text" colspan="5">
-          No records could be found at this time.
-        </td>
-      </tr>
-      {:else}
-      {#each records as r, _}
-      <tr on:click={() => visitRecord(r)} class="record">
-        <td>{r.game_mode}</td>
-        <td>
-          <div class="flex flex-row">
-            <div>
-              <GameTileFromString --height="var(--base-font-size)" spec="{r.tile}" showBackground={false} />
+<div class="flex flex-row my-4 justify-center">
+  <div class="w-full px-2 lg:w-10/12">
+    <table class="border-x border-b w-full">
+      <thead class="text-left">
+        <tr>
+          <th scope="col">Game mode</th>
+          <th scope="col">Character</th>
+          <th class="hidden lg:table-cell" scope="col">Score</th>
+          <th class="hidden lg:table-cell" scope="col">Turns</th>
+          <th scope="col">Uploaded</th>
+        </tr>
+      </thead>
+      <tbody class="text-left">
+        {#if waitingForRecords}
+        <!-- Records haven't been retrieved yet -->
+        <tr>
+          <td class="text-center" colspan="5">
+            <Spinner>
+              Loading game records...
+            </Spinner>
+          </td>
+        </tr>
+        {/if}
+        {#if listRecordsPromise !== null}
+        {#await listRecordsPromise then records}
+        {#if records.length === 0}
+        <tr>
+          <td class="multicol-text" colspan="5">
+            No records could be found at this time.
+          </td>
+        </tr>
+        {:else}
+        {#each records as r, _}
+        <tr on:click={() => visitRecord(r)} class="record">
+          <td>{r.game_mode}</td>
+          <td>
+            <div class="flex flex-row">
+              <div>
+                <GameTileFromString --height="var(--base-font-size)" spec="{r.tile}" showBackground={false} />
+              </div>
+              <div class="character-name text-wrap mw-96 pl-2">
+                <span class="d-inline-block text-truncate">
+                  <ColorizedText text="{r.character_name}" />
+                </span>
+              </div>
             </div>
-            <div class="character-name text-wrap">
-              <span class="d-inline-block text-truncate">
-                <ColorizedText text="{r.character_name}" />
-              </span>
-            </div>
-          </div>
-        </td>
-        <td class="hidden lg:table-cell">{r.score}</td>
-        <td class="hidden lg:table-cell">{r.turns}</td>
-        <td><DateTime timestamp="{r.created}" /></td>
-      </tr>
-      {/each}
-      {/if}
-      {/await}
-      {/if}
-    </tbody>
-  </table>
+          </td>
+          <td class="hidden lg:table-cell">{r.score}</td>
+          <td class="hidden lg:table-cell">{r.turns}</td>
+          <td><DateTime showDelta={true} timestamp="{r.created}" /></td>
+        </tr>
+        {/each}
+        {/if}
+        {/await}
+        {/if}
+      </tbody>
+    </table>
+
+    <div class="flex flex-row mt-2 px-4">
+      <button type="button" class="hover:bg-emerald-900 rounded-md p-2 w-32"
+        disabled={currentIndex < numRetrieved}
+        on:click={() => null}>
+        <i class="bi-arrow-left-square-fill"></i> Previous
+      </button>
+      <div class="flex-auto"></div>
+      <button type="button" class="hover:bg-emerald-900 rounded-md p-2 w-32"
+        disabled={currentIndex + numRetrieved >= count}
+        on:click={() => null}>
+        Next <i class="bi-arrow-right-square-fill"></i>
+      </button>
+    </div>
+  </div>
 </div>
